ede7bbec4911aa9ed0485eb3d410545db88a7a58
Ole Krüger <ole.kruger@trili.tech>
2025-06-25 10:32:35 +0100

    Purge old builder code

--- src/riscv/lib/src/jit/builder/errno.rs
+++ src/riscv/lib/src/jit/builder/errno.rs
@@ -8,37 +8,10 @@
 //! `1` for failure or `0` for success.
 //!
 //! Any returned values are written via 'out-pointers' - and should only be
-//! loaded on success. [`Errno`] provides a wrapper for this mechanism,
-//! that ensures safety.
+//! loaded on success.
 
 use cranelift::codegen::ir;
-use cranelift::codegen::ir::InstBuilder;
 use cranelift::frontend::FunctionBuilder;
-
-use crate::interpreter::atomics::ReservationSetOption;
-use crate::interpreter::atomics::reset_reservation_set;
-use crate::jit::state_access::JitStateAccess;
-use crate::machine_state::memory::MemoryConfig;
-use crate::state_backend::ManagerBase;
-
-/// Wrapper for `Errno`-type returns.
-///
-/// This is formed of two parts, the 'code', and a output function.
-/// The output function will only be called on success - ie any
-/// out parameters (with the exception of the exception ptr) - can be assumed
-/// to be initialised, if the externally-called function guarantees it.
-///
-/// *NB* a trait is used, rather than a structure, to allow us to naturally use
-/// _unboxed closures_ for the function that returns the output values, without needing
-/// to thread extra generics.
-pub(crate) trait Errno<T, MC: MemoryConfig, M: ManagerBase> {
-    /// Insert exception handling branch that will be triggered at runtime
-    /// when `errno` indicates failure.
-    ///
-    /// The caller of this function is put back into the context of the happy path -
-    /// ie, where `errno` indicates success and no exception occurred.
-    fn handle(self, builder: &mut super::Builder<'_, MC, M>) -> T;
-}
 
 /// Helper type for ensuring fallible operations are handled correctly.
 ///
@@ -47,9 +20,6 @@ pub(crate) trait Errno<T, MC: MemoryConfig, M: ManagerBase> {
 /// - if yes, the pointer to the exception in memory that has been written with the failure kind
 /// - if no, a handler to load any state that was returned in `out-params` that is now safe to
 ///   access.
-///
-/// The only way to access the values that will be returned on success, is via the
-/// [`Errno::handle`] method.
 pub(crate) struct ErrnoImpl<T, F>
 where
     F: FnOnce(&mut FunctionBuilder<'_>) -> T,
@@ -75,98 +45,5 @@ where
             exception_ptr,
             on_ok,
         }
-    }
-}
-
-impl<T, F, MC: MemoryConfig, JSA: JitStateAccess> Errno<T, MC, JSA> for ErrnoImpl<T, F>
-where
-    F: FnOnce(&mut FunctionBuilder<'_>) -> T,
-{
-    fn handle(self, builder: &mut super::Builder<'_, MC, JSA>) -> T {
-        let errno_code = self.is_exception;
-
-        let on_error = builder.builder.create_block();
-        let on_ok = builder.builder.create_block();
-
-        builder
-            .builder
-            .ins()
-            .brif(errno_code, on_error, &[], on_ok, &[]);
-
-        let snapshot = builder.dynamic.clone();
-
-        builder.builder.switch_to_block(on_error);
-        // Handle the exception in the 'error occurred' block
-        builder.handle_exception(self.exception_ptr);
-
-        builder.builder.seal_block(on_error);
-
-        // Go back to the 'no error occurred' block, any returned
-        // values are ok to load
-        builder.builder.switch_to_block(on_ok);
-        builder.dynamic = snapshot;
-
-        (self.on_ok)(&mut builder.builder)
-    }
-}
-
-/// Helper type for producing the required IR for the address alignment check at the beginning
-/// of an atomic operation. In the case of a store_conditional, this will also reset the reservation.
-///
-/// The only way to access the values that will be returned on success, is via the
-/// [`Errno::handle`] method.
-pub(crate) struct AtomicAccessGuard {
-    errno: ir::Value,
-    address: ir::Value,
-    reservation_set_option: ReservationSetOption,
-}
-
-impl AtomicAccessGuard {
-    /// Construct a new `Errno` that must be handled.
-    pub(crate) fn new(
-        errno: ir::Value,
-        address: ir::Value,
-        reservation_set_option: ReservationSetOption,
-    ) -> Self {
-        Self {
-            errno,
-            address,
-            reservation_set_option,
-        }
-    }
-}
-
-impl<MC: MemoryConfig, JSA: JitStateAccess> Errno<(), MC, JSA> for AtomicAccessGuard {
-    fn handle(self, builder: &mut super::Builder<'_, MC, JSA>) {
-        let error_branch = builder.builder.create_block();
-        let fallthrough = builder.builder.create_block();
-
-        builder
-            .builder
-            .ins()
-            .brif(self.errno, error_branch, &[], fallthrough, &[]);
-
-        // both IR blocks need access to the dynamic values at this point in time.
-        // These are modified by the jump to the end block below, and possibly by the
-        // `on_branching` function.
-        let snapshot = builder.dynamic.clone();
-
-        builder.builder.switch_to_block(error_branch);
-
-        let exception_ptr = builder
-            .jsa_call
-            .raise_store_amo_access_fault_exception(&mut builder.builder, self.address);
-        if let ReservationSetOption::Reset = self.reservation_set_option {
-            // If the atomic operation was a store_conditional, we reset the reservation.
-            reset_reservation_set(builder);
-        }
-
-        builder.handle_exception(exception_ptr);
-
-        builder.builder.seal_block(error_branch);
-
-        // Restore the dynamic values to the branching point, for the fallthrough block.
-        builder.dynamic = snapshot;
-        builder.builder.switch_to_block(fallthrough)
     }
 }
