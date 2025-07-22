dcbdf57b1b9267c2e42c62f3097fd7c29b1770d0
Neelay Sant <neelay.sant@trili.tech>
2025-07-22 11:10:43 +0100

    feat(jit): add recursion info to sequence analysis

--- src/riscv/lib/src/jit/builder/sequence/sequence_analysis.rs
+++ src/riscv/lib/src/jit/builder/sequence/sequence_analysis.rs
@@ -3,6 +3,7 @@
 //! `Sequence Analysis` is currently only used in tests and has not yet been integrated into the JIT.
 #![cfg(test)]
 
+use std::collections::HashSet;
 use std::fmt::Debug;
 
 use cranelift::prelude::Block;
@@ -54,11 +55,19 @@ struct OutgoingInfo<B: BlockRepr, OC: OutcomeRepr> {
     external_dests: Vec<OC>,
 }
 
+/// Instruction information classifying internal-outgoing edges as simple or recursive.
+#[derive(Clone, Debug)]
+struct RecursionInfo<B: BlockRepr> {
+    simple_transitions: Vec<(InstrId, B)>,
+    recursive_transitions: Vec<(InstrId, B)>,
+}
+
 /// Information built from sequence analysis for managing intra-sequence jumps.
 #[derive(Clone, Debug)]
 pub struct SequenceInfo<B: BlockRepr, OC: OutcomeRepr> {
     addr_map: AddrMap,
     graph: InstrMap<(IncomingInfo, OutgoingInfo<B, OC>)>,
+    recursions_info: InstrMap<RecursionInfo<B>>,
 }
 
 impl<B: BlockRepr, OC: OutcomeRepr> SequenceInfo<B, OC> {
@@ -97,7 +106,37 @@ impl<B: BlockRepr, OC: OutcomeRepr> SequenceInfo<B, OC> {
 
         resolve_outcomes::<B, OC>(&addr_map, instrs, &mut graph);
 
-        Self { addr_map, graph }
+        let create_recursive_info = RecursionInfo {
+            simple_transitions: vec![],
+            recursive_transitions: vec![],
+        };
+
+        let recursions_info: InstrMap<RecursionInfo<B>> =
+            graph.clone().map(|_, _| create_recursive_info.clone());
+
+        Self {
+            addr_map,
+            graph,
+            recursions_info,
+        }
+    }
+
+    /// Populate the recursion data for the sequence graph.
+    pub fn populate_recursion_data(&mut self) {
+        let mut recursions_info = self.recursions_info.clone();
+        let mut instrs_seen = HashSet::new();
+
+        let outgoing_graph = self.graph.clone().map(|_, (_, outgoing)| outgoing.clone());
+        for (instr, _) in self.graph.iter() {
+            resolve_recursions::<B, OC>(
+                &outgoing_graph,
+                &mut recursions_info,
+                &mut instrs_seen,
+                instr,
+            );
+        }
+
+        self.recursions_info = recursions_info;
     }
 }
 
@@ -138,8 +177,59 @@ fn resolve_outcomes<B: BlockRepr, OC: OutcomeRepr>(
     }
 }
 
+/// Traverse the sequence graph from the current instruction to classify
+/// all edges as simple or recursive in the [`RecursionInfo`].
+///
+/// A recursive edge is one that forms part of a loop in the graph. Only one
+/// edge in the loop is classified as recursive, with the rest being simple edges.
+fn resolve_recursions<B: BlockRepr, OC: OutcomeRepr>(
+    graph: &InstrMap<OutgoingInfo<B, OC>>,
+    recursions: &mut InstrMap<RecursionInfo<B>>,
+    instrs_seen: &mut HashSet<InstrId>,
+    curr_instr: InstrId,
+) {
+    if !instrs_seen.insert(curr_instr) {
+        return;
+    }
+
+    let mut paths = vec![vec![curr_instr]];
+    while let Some(path) = paths.pop() {
+        let Some(source_instr_id) = path.last() else {
+            panic!("A path cannot be empty.");
+        };
+
+        let source_instr = &graph[*source_instr_id];
+
+        for dest_instr in &source_instr.internal_dests {
+            if path.contains(&dest_instr.0) {
+                // This instruction has been seen in the current path.
+                // Hence, we have a recursion.
+                recursions[*source_instr_id]
+                    .recursive_transitions
+                    .push(dest_instr.clone());
+                // Do not explore this path further as it has recursed.
+                continue;
+            }
+
+            if instrs_seen.insert(dest_instr.0) {
+                // We have not seen this destination instruction before.
+                // Hence, we must explore this new path.
+                let mut new_path = path.clone();
+                new_path.push(dest_instr.0);
+                paths.push(new_path);
+            }
+
+            recursions[*source_instr_id]
+                .simple_transitions
+                .push(dest_instr.clone());
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use proptest::collection::vec;
+    use proptest::prelude::Strategy;
     use proptest::proptest;
 
     use super::*;
@@ -160,8 +250,16 @@ mod tests {
     }
 
     impl<B: BlockRepr, OC: OutcomeRepr> SequenceInfo<B, OC> {
-        fn set(addr_map: AddrMap, graph: InstrMap<(IncomingInfo, OutgoingInfo<B, OC>)>) -> Self {
-            Self { addr_map, graph }
+        fn set(
+            addr_map: AddrMap,
+            graph: InstrMap<(IncomingInfo, OutgoingInfo<B, OC>)>,
+            recursions_info: InstrMap<RecursionInfo<B>>,
+        ) -> Self {
+            Self {
+                addr_map,
+                graph,
+                recursions_info,
+            }
         }
     }
 
@@ -206,6 +304,34 @@ mod tests {
             other_externals.sort();
 
             self_internals == other_internals && self_externals == other_externals
+        }
+    }
+
+    impl<B: BlockRepr> RecursionInfo<B> {
+        fn set(
+            simple_transitions: Vec<(InstrId, B)>,
+            recursive_transitions: Vec<(InstrId, B)>,
+        ) -> Self {
+            Self {
+                simple_transitions,
+                recursive_transitions,
+            }
+        }
+    }
+
+    impl PartialEq for RecursionInfo<()> {
+        fn eq(&self, other: &Self) -> bool {
+            let mut self_simples = self.simple_transitions.clone();
+            let mut other_simples = other.simple_transitions.clone();
+            self_simples.sort_by_key(|id| id.0.id());
+            other_simples.sort_by_key(|id| id.0.id());
+
+            let mut self_recursives = self.recursive_transitions.clone();
+            let mut other_recursives = other.recursive_transitions.clone();
+            self_recursives.sort_by_key(|id| id.0.id());
+            other_recursives.sort_by_key(|id| id.0.id());
+
+            self_simples == other_simples && self_recursives == other_recursives
         }
     }
 
@@ -332,13 +458,19 @@ mod tests {
         ];
         let lowered_instrs = create_lowered_instructions(&mut sequence_builder, instrs);
 
-        let sequence_info = SequenceInfo::<(), ()>::from_lowered_instructions(&lowered_instrs);
+        let mut sequence_info = SequenceInfo::<(), ()>::from_lowered_instructions(&lowered_instrs);
         assert_eq!(
             sequence_info.addr_map.addresses().len(),
             lowered_instrs.len()
         );
         assert_eq!(
             sequence_info.graph.instructions().len(),
+            lowered_instrs.len()
+        );
+
+        sequence_info.populate_recursion_data();
+        assert_eq!(
+            sequence_info.recursions_info.instructions().len(),
             lowered_instrs.len()
         );
 
@@ -360,6 +492,15 @@ mod tests {
             OutgoingInfo::set(vec![], vec![()]),
         ];
 
+        let expected_recursions: Vec<RecursionInfo<()>> = vec![
+            RecursionInfo::set(vec![(InstrId::set(1), ())], vec![]),
+            RecursionInfo::set(vec![(InstrId::set(2), ())], vec![]),
+            RecursionInfo::set(vec![(InstrId::set(3), ()), (InstrId::set(4), ())], vec![]),
+            RecursionInfo::set(vec![], vec![(InstrId::set(1), ())]),
+            RecursionInfo::set(vec![(InstrId::set(5), ())], vec![]),
+            RecursionInfo::set(vec![], vec![]),
+        ];
+
         let expected_seq_info = SequenceInfo::set(
             sequence_info.addr_map.clone(),
             InstrMap::set(
@@ -368,6 +509,7 @@ mod tests {
                     .zip(expected_outgoings)
                     .collect(),
             ),
+            InstrMap::set(expected_recursions),
         );
 
         // test that the generated sequence information matches the expected information.
@@ -380,6 +522,12 @@ mod tests {
             assert_eq!(
                 outgoing, expected_outgoing,
                 "Mismatched outgoing at instr_id {instr_id:?}"
+            );
+            let expected_recursion = &expected_seq_info.recursions_info[instr_id];
+            let actual_recursion = &sequence_info.recursions_info[instr_id];
+            assert_eq!(
+                actual_recursion, expected_recursion,
+                "Mismatched recursion at instr_id {instr_id:?}"
             );
         }
     }
@@ -403,13 +551,19 @@ mod tests {
         ];
         let lowered_instrs = create_lowered_instructions(&mut sequence_builder, instrs);
 
-        let sequence_info = SequenceInfo::<(), ()>::from_lowered_instructions(&lowered_instrs);
+        let mut sequence_info = SequenceInfo::<(), ()>::from_lowered_instructions(&lowered_instrs);
         assert_eq!(
             sequence_info.addr_map.addresses().len(),
             lowered_instrs.len()
         );
         assert_eq!(
             sequence_info.graph.instructions().len(),
+            lowered_instrs.len()
+        );
+
+        sequence_info.populate_recursion_data();
+        assert_eq!(
+            sequence_info.recursions_info.instructions().len(),
             lowered_instrs.len()
         );
 
@@ -435,6 +589,17 @@ mod tests {
             OutgoingInfo::set(vec![], vec![()]),
         ];
 
+        let expected_recursions: Vec<RecursionInfo<()>> = vec![
+            RecursionInfo::set(vec![(InstrId::set(1), ())], vec![]),
+            RecursionInfo::set(vec![(InstrId::set(2), ())], vec![]),
+            RecursionInfo::set(vec![(InstrId::set(3), ()), (InstrId::set(4), ())], vec![]),
+            RecursionInfo::set(vec![(InstrId::set(6), ())], vec![]),
+            RecursionInfo::set(vec![(InstrId::set(5), ())], vec![(InstrId::set(1), ())]),
+            RecursionInfo::set(vec![(InstrId::set(6), ())], vec![]),
+            RecursionInfo::set(vec![(InstrId::set(7), ())], vec![]),
+            RecursionInfo::set(vec![], vec![]),
+        ];
+
         let expected_seq_info = SequenceInfo::set(
             sequence_info.addr_map.clone(),
             InstrMap::set(
@@ -443,6 +608,7 @@ mod tests {
                     .zip(expected_outgoings)
                     .collect(),
             ),
+            InstrMap::set(expected_recursions),
         );
 
         // test that the generated sequence information matches the expected information.
@@ -455,6 +621,13 @@ mod tests {
             assert_eq!(
                 outgoing, expected_outgoing,
                 "Mismatched outgoing at instr_id {instr_id:?}"
+            );
+
+            let expected_recursion = &expected_seq_info.recursions_info[instr_id];
+            let actual_recursion = &sequence_info.recursions_info[instr_id];
+            assert_eq!(
+                actual_recursion, expected_recursion,
+                "Mismatched recursion at instr_id {instr_id:?}"
             );
         }
     }
@@ -491,6 +664,79 @@ mod tests {
                     (sum_incomings + num_incomings, sum_outgoings + num_outgoings)
                 });
             assert_eq!(total_incomings, total_outgoings + 1); // +1 for the implicit incoming to the first instruction.
+        }
+    }
+
+    fn outgoing_info_strategy(size: usize) -> impl Strategy<Value = OutgoingInfo<(), ()>> {
+        vec(0..size, 0..3).prop_map(|node_indices| {
+            let internal_dests = node_indices
+                .iter()
+                .map(|idx| (InstrId::set(*idx), ()))
+                .collect();
+            OutgoingInfo {
+                internal_dests,
+                external_dests: Vec::new(),
+            }
+        })
+    }
+
+    fn create_outgoing_instrmap() -> impl Strategy<Value = InstrMap<OutgoingInfo<(), ()>>> {
+        (1..10_usize).prop_flat_map(|size| {
+            vec(outgoing_info_strategy(size), size..=size).prop_map(move |outgoings| {
+                let mut instr_builder = InstrMapBuilder::new();
+                for (i, outgoing) in outgoings.into_iter().enumerate() {
+                    instr_builder.insert(i as u64, outgoing);
+                }
+                let (_, instr_map) = instr_builder.build();
+                instr_map
+            })
+        })
+    }
+
+    proptest! {
+        #[test]
+        fn test_recursion(graph in create_outgoing_instrmap()) {
+            let create_recursive_info = RecursionInfo::<()> {
+                simple_transitions: vec![],
+                recursive_transitions: vec![],
+            };
+
+            let mut recursions_info: InstrMap<RecursionInfo<()>> =
+                graph.clone().map(|_, _| create_recursive_info.clone());
+
+            let mut instrs_seen = HashSet::new();
+
+            for (instr, _) in graph.iter() {
+                resolve_recursions(
+                    &graph,
+                    &mut recursions_info,
+                    &mut instrs_seen,
+                    instr,
+                );
+            }
+
+            // ensure every instruction has been seen
+            for (instr_id, _) in graph.iter() {
+                assert!(instrs_seen.contains(&instr_id));
+            }
+
+            // Ensure the total number of simple and recursive transitions matches the total number of internal outgoings.
+            let (total_simple, total_recursive, total_outgoings): (usize, usize, usize) = graph.iter()
+                .map(|(instr_id, outgoing)| {
+                    let rec_info = &recursions_info[instr_id];
+                    (rec_info.simple_transitions.len(), rec_info.recursive_transitions.len(), outgoing.internal_dests.len())
+                })
+                .fold((0, 0, 0), |(sum_simple, sum_recursive, sum_outgoings), (num_simple, num_recursive, num_outgoings)| {
+                    (sum_simple + num_simple, sum_recursive + num_recursive, sum_outgoings + num_outgoings)
+                });
+            assert_eq!(total_simple + total_recursive, total_outgoings);
+
+            for instr in recursions_info.iter() {
+                // Ensure no overlap between simple and recursive transitions.
+                for simple in &instr.1.simple_transitions {
+                    assert!(!instr.1.recursive_transitions.iter().any(|rec| rec.0 == simple.0));
+                }
+            }
         }
     }
 }
