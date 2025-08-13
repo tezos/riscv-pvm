74c684a97f9110e149e8980e6d9f3efe14fdc15e
Kurtis Charnock <kurtis.charnock@trili.tech>
2025-08-13 14:00:04 +0100

    Assorted fixes (#247)
    * RISC-V: Remove defunct conditional compilation comments
    
    * RISC-V: Fix typo

--- src/riscv/lib/src/pvm/common.rs
+++ src/riscv/lib/src/pvm/common.rs
@@ -223,7 +223,6 @@ impl<MC: MemoryConfig, BCC: BlockCacheConfig, B: block::Block<MC, M>, M: state_b
     }
 
     /// Handle an exception using the defined Execution Environment.
-    // The conditional compilation below causes some warnings.
     fn handle_exception(&mut self, hooks: impl PvmHooks, _exception: EnvironException) -> bool
     where
         M: state_backend::ManagerReadWrite,
@@ -265,11 +264,10 @@ impl<MC: MemoryConfig, BCC: BlockCacheConfig, B: block::Block<MC, M>, M: state_b
     ///       See section 3.3.1 for context on retired instructions.
     /// e.g: a load instruction raises an exception but the first instruction
     /// of the trap handler will be executed and retired,
-    /// so in the end the load instruction which does not bubble it's exception up to
+    /// so in the end the load instruction which does not bubble its exception up to
     /// the execution environment will still retire an instruction, just not itself.
     /// (a possible case: the privilege mode access violation is treated in EE,
     /// but a page fault is not)
-    // The conditional compilation below causes some warnings.
     pub(crate) fn eval_max(&mut self, mut hooks: impl PvmHooks, step_bounds: Bound<usize>) -> usize
     where
         M: state_backend::ManagerReadWrite,
