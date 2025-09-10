9c7a59e54dc1ef7561e88fa1415ab09dec1cbb27
Edwin Fernando <edwin.fernando@nomadic-labs.com>
2025-09-10 17:26:06 +0100

    feat(par_sys_call): implement Elem for bool

--- src/riscv/lib/src/state_backend/elems.rs
+++ src/riscv/lib/src/state_backend/elems.rs
@@ -87,6 +87,23 @@ impl_dyn_value_prim!(i64);
 impl_dyn_value_prim!(u128);
 impl_dyn_value_prim!(i128);
 
+impl Elem for bool {
+    const STORED_SIZE: NonZeroUsize =
+        NonZeroUsize::new(std::mem::size_of::<bool>()).expect("Type has zero size");
+
+    #[inline]
+    unsafe fn read_unaligned(source: *const u8) -> Self {
+        (unsafe { u8::read_unaligned(source) } != 0)
+    }
+
+    #[inline]
+    unsafe fn write_unaligned(self, dest: *mut u8) {
+        unsafe {
+            (if self { 1 } else { 0 }).write_unaligned(dest);
+        }
+    }
+}
+
 impl<E: Elem, const LEN: usize> Elem for [E; LEN] {
     const STORED_SIZE: NonZeroUsize = {
         let len = NonZeroUsize::new(LEN).expect("Array length must be non-zero");
