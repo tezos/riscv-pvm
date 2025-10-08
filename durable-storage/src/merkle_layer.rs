97f703c0659e53d9a4fd1e5306e2d1160baf0d2e
Kurtis Charnock <kurtis.charnock@trili.tech>
2025-10-08 15:09:28 +0100

    feat(dursto): key comparison
    Makes the `Key` type shape explicit and implements comparison and
    equality.

--- durable-storage/src/merkle_layer.rs
+++ durable-storage/src/merkle_layer.rs
@@ -2,11 +2,42 @@
 //
 // SPDX-License-Identifier: MIT
 
+use std::cmp::Ordering;
+
 /// An identifier generated for a given commit.
 pub struct CommitId;
 
 /// A unique key used to store, retrieve and mutate data in durable storage.
-pub struct Key;
+pub struct Key([u8; KEY_LENGTH]);
+
+impl Eq for Key {}
+
+impl Ord for Key {
+    fn cmp(&self, other: &Key) -> Ordering {
+        for (l, r) in self.0.iter().zip(other.0.iter()) {
+            let comparison = l.cmp(r);
+            if comparison != Ordering::Equal {
+                return comparison;
+            }
+        }
+
+        Ordering::Equal
+    }
+}
+
+impl PartialEq for Key {
+    fn eq(&self, other: &Key) -> bool {
+        self.cmp(other) == Ordering::Equal
+    }
+}
+
+impl PartialOrd for Key {
+    fn partial_cmp(&self, other: &Key) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+const KEY_LENGTH: usize = 32;
 
 /// Errors for fallible [MerkleLayer] operations.
 pub enum MerkleLayerError {}
@@ -52,3 +83,24 @@ pub trait MerkleLayerStable: Clone + Sized {
 
 /// A stand-in for the in-development layer for persisting data to durable storage.
 pub struct PersistenceLayer;
+
+#[cfg(test)]
+mod tests {
+    use std::cmp::Ordering;
+
+    use super::KEY_LENGTH;
+    use super::Key;
+
+    #[test]
+    fn test_key_comparison() {
+        let mut key1: Key = Key([0; KEY_LENGTH]);
+        let mut key2: Key = Key([0; KEY_LENGTH]);
+
+        assert_eq!(key1, key2);
+        assert_eq!(key1.cmp(&key2), Ordering::Equal);
+        key1.0[1] = 1;
+        assert_eq!(key1.cmp(&key2), Ordering::Greater);
+        key2.0[0] = 1;
+        assert_eq!(key1.cmp(&key2), Ordering::Less);
+    }
+}
