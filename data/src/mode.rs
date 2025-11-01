8a8b3681686f6f6d9af0b8a820dde0db36edd31b
Ole Krüger <ole.kruger@trili.tech>
2025-11-01 23:04:50 +0000

    feat(data): implement Modal traits for Modes

--- data/src/mode.rs
+++ data/src/mode.rs
@@ -13,10 +13,22 @@
 use std::convert::Infallible;
 use std::marker::PhantomData;
 
+/// Operational mode of the PVM
+///
+/// This trait can be used to instantiate the modal representation for a [`Modal`] template type.
+pub trait Mode {
+    /// Select the type from the [`Modal`] template that corresponds to the current mode.
+    type Select<Template: Modal + ?Sized>;
+}
+
 /// Normal execution mode
 ///
 /// The PVM advances its state as quickly as possible in this mode.
 pub enum Normal {}
+
+impl Mode for Normal {
+    type Select<Template: Modal + ?Sized> = Template::Normal;
+}
 
 /// Proof generation mode
 ///
@@ -25,7 +37,70 @@ pub enum Normal {}
 /// PVM semantics.
 pub struct Prove<'normal>(PhantomData<&'normal Normal>, Infallible);
 
+impl<'normal> Mode for Prove<'normal> {
+    type Select<Template: Modal + ?Sized> = Template::Prove<'normal>;
+}
+
 /// Proof verification mode
 ///
 /// In this mode, the PVM verifies a proof that was generated in the [`Prove`] mode.
 pub enum Verify {}
+
+impl Mode for Verify {
+    type Select<Template: Modal + ?Sized> = Template::Verify;
+}
+
+/// Proxy template for modal representations
+///
+/// Types that need to exhibit different representations depending on the PVM's operational mode
+/// can use this trait via a template type to define their mode-specific internal representation.
+///
+/// # Example
+///
+/// In the following example, `MyType` needs to have different internal representations. It uses
+/// `MyTemplate` as a modal template to define these representations for each mode. The type
+/// `M::Select<MyTemplate>` will resolve to the appropriate representation based on the mode `M`.
+///
+/// This results in `MyType<Normal>` having a `MyNormalType` representation, `MyType<Prove>` having
+/// a `MyProveType` representation, and `MyType<Verify>` having a `MyVerifyType` representation.
+///
+/// ```
+/// use octez_riscv_data::mode::*;
+///
+/// enum MyTemplate {}
+///
+/// impl Modal for MyTemplate {
+///     type Normal = MyNormalType;
+///
+///     type Prove<'normal> = MyProveType;
+///
+///     type Verify = MyVerifyType;
+/// }
+///
+/// struct MyNormalType {
+///     // ...
+/// }
+///
+/// struct MyProveType {
+///     // ...
+/// }
+///
+/// struct MyVerifyType {
+///     // ...
+/// }
+///
+/// #[repr(transparent)]
+/// struct MyType<M: Mode> {
+///     repr: M::Select<MyTemplate>,
+/// }
+/// ```
+pub trait Modal {
+    /// Representation in [`Normal`] mode
+    type Normal;
+
+    /// Representation in [`Prove`] mode
+    type Prove<'normal>;
+
+    /// Representation in [`Verify`] mode
+    type Verify;
+}
