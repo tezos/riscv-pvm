96d616984397eafd6aa2257f27f83a6e05357ddf
Ole Krüger <ole.kruger@trili.tech>
2025-11-29 22:32:05 +0000

    refactor: remove unused code after ProofLayout removal

--- src/riscv/lib/src/array_utils.rs
+++ src/riscv/lib/src/array_utils.rs
@@ -2,19 +2,6 @@
 //
 // SPDX-License-Identifier: MIT
 
-/// Return a boxed array without first allocating it on the stack
-macro_rules! boxed_array {
-    ($elem:expr; $n:expr) => {
-        Box::try_from(vec![$elem; $n])
-            .map_err(|_| {
-                unreachable!("Converting a vector to an array of the same size always succeeds")
-            })
-            .unwrap()
-    };
-}
-
-pub(crate) use boxed_array;
-
 /// Create a boxed array from a function.
 pub fn boxed_from_fn<T, const LEN: usize>(mut f: impl FnMut() -> T) -> Box<[T; LEN]> {
     let mut entries = Vec::with_capacity(LEN);
--- src/riscv/lib/src/state_backend/proof_layout.rs
+++ src/riscv/lib/src/state_backend/proof_layout.rs
@@ -6,21 +6,13 @@
 use std::error;
 
 use bincode::error::DecodeError;
-use bincode::error::EncodeError;
-use octez_riscv_data::hash::Hash;
 use octez_riscv_data::merkle_proof::DeserialiserError;
 use octez_riscv_data::merkle_proof::Partial;
 use octez_riscv_data::merkle_proof::proof_tree::MerkleProof;
-use octez_riscv_data::merkle_proof::proof_tree::MerkleProofLeaf;
 use octez_riscv_data::merkle_proof::tag::InvalidTagError;
-use octez_riscv_data::mode::Verify;
-use octez_riscv_data::tree::Tree;
 use perfect_derive::perfect_derive;
 
-use super::Layout;
 use super::proof_backend::merkle::MERKLE_ARITY;
-use super::proof_backend::proof::deserialiser::Result;
-use crate::array_utils::boxed_array;
 use crate::state_backend::proof_backend::proof::NotEnoughBytesError;
 
 /// Errors occurring when parsing the tag structure of a Merkle proof.
@@ -51,9 +43,6 @@ pub enum ProofError {
     #[error("Proof tree is absent")]
     AbsentProof,
 
-    #[error("A part of the proof required to parse further is absent")]
-    DependentNodeIsAbsent,
-
     #[error("Encountered a node with a bad number of branches: expected {expected}, got {got}")]
     BadNumberOfBranches { expected: usize, got: usize },
 
@@ -80,33 +69,6 @@ impl DeserialiserError for ProofError {
     }
 }
 
-/// Regions for the verifier backend for a specific layout.
-pub type VerifyAlloc<L> = <L as Layout>::Allocated<Verify>;
-
-/// Errors that may occur when hashing a state in [`Verify`] mode
-#[derive(Debug, thiserror::Error)]
-pub enum PartialHashError {
-    /// The hash could not be computed because encoding a value to bytes failed. The byte
-    /// representation is used as input to the hash function.
-    #[error("Error while encoding a to-be-hashed value: {0}")]
-    Encode(#[from] EncodeError),
-
-    #[error("Error from proof: {0}")]
-    FromProof(#[from] ProofError),
-
-    /// Indicates that a hash could not be computed due to absent data,
-    /// but from which it is possible to recover if the level at which
-    /// it was raised is part of a blinded subtree and its hash is present
-    /// in the proof.
-    #[error("Potentially recoverable error")]
-    PotentiallyRecoverable,
-
-    /// Indicates that a hash could not be computed because the data being
-    /// hashed is only partially available.
-    #[error("Fatal error")]
-    Fatal,
-}
-
 /// Part of a tree that may be absent
 #[derive(Debug, PartialEq)]
 #[perfect_derive(Clone, Copy)]
@@ -120,101 +82,6 @@ pub enum ProofPart<'a, T: ?Sized> {
 
 /// Part of a Merkle proof tree
 pub type ProofTree<'a> = ProofPart<'a, MerkleProof>;
-
-impl<'a> ProofTree<'a> {
-    /// Interpret this part of the Merkle proof as a node with `LEN` branches.
-    pub fn into_branches<const LEN: usize>(self) -> Result<Box<[Self; LEN]>> {
-        let ProofTree::Present(proof) = self else {
-            // The requested branches are not represented in the Merkle proof at all, not even
-            // through a blinded node.
-            return Ok(boxed_array![ProofTree::Absent; LEN]);
-        };
-
-        match proof {
-            Tree::Node(branches) => {
-                let branches: &[MerkleProof; LEN] =
-                    branches.as_slice().try_into().map_err(|_| {
-                        ProofError::BadNumberOfBranches {
-                            got: branches.len(),
-                            expected: LEN,
-                        }
-                    })?;
-                Ok(branches
-                    .iter()
-                    .map(ProofTree::Present)
-                    .collect::<Vec<_>>()
-                    .try_into()
-                    .map_err(|_| {
-                        unreachable!(
-                            "Converting a vector to an array of the same size always succeeds"
-                        )
-                    })
-                    .unwrap())
-            }
-
-            Tree::Leaf(leaf) => match leaf {
-                MerkleProofLeaf::Blind(_hash) => Ok(boxed_array![ProofTree::Absent; LEN]),
-                _ => Err(ProofError::UnexpectedLeaf)?,
-            },
-        }
-    }
-
-    /// Interpret this part of the Merkle proof as a leaf.
-    pub fn into_leaf(self) -> Result<ProofPart<'a, [u8]>> {
-        if let ProofTree::Present(proof) = self {
-            match proof {
-                Tree::Node(_) => Err(ProofError::UnexpectedNode),
-                Tree::Leaf(leaf) => match leaf {
-                    MerkleProofLeaf::Blind(_) => Ok(ProofPart::Absent),
-                    MerkleProofLeaf::Read(data) => Ok(ProofPart::Present(data.as_slice())),
-                },
-            }
-        } else {
-            Ok(ProofPart::Absent)
-        }
-    }
-
-    /// For the purpose of computing the final hash of a state in `Verify` mode,
-    /// if present, try to interpret this part of a Merkle proof as:
-    /// - a node with `LEN` branches, in which case return the proof branches
-    ///   and no proof hash
-    /// - a blinded leaf which corresponds to a node with `LEN` children,
-    ///   in which case return absent branches and the proof hash
-    ///
-    /// If the proof tree is absent, return absent branches and no proof hash.
-    pub fn into_branches_with_hash<const LEN: usize>(
-        self,
-    ) -> Result<(Box<[ProofTree<'a>; LEN]>, Option<Hash>), PartialHashError> {
-        let ProofTree::Present(proof) = self else {
-            return Ok((boxed_array![ProofTree::Absent; LEN], None));
-        };
-
-        match proof {
-            Tree::Node(branches) if branches.len() != LEN => Err(PartialHashError::FromProof(
-                ProofError::BadNumberOfBranches {
-                    got: branches.len(),
-                    expected: LEN,
-                },
-            )),
-            Tree::Node(branches) => Ok((
-                branches
-                    .iter()
-                    .map(ProofTree::Present)
-                    .collect::<Vec<_>>()
-                    .into_boxed_slice()
-                    .try_into()
-                    .map_err(|_| PartialHashError::Fatal)?,
-                None,
-            )),
-            Tree::Leaf(leaf) => match leaf {
-                MerkleProofLeaf::Blind(hash) => {
-                    Ok((boxed_array![ProofTree::Absent; LEN], Some(*hash)))
-                }
-                _ => Err(ProofError::UnexpectedLeaf)?,
-            },
-        }
-    }
-}
 
 /// Similar to [`ProofPart`], but owns the underlying [`MerkleProof`].
 #[derive(Debug, Clone)]
@@ -232,15 +99,6 @@ impl OwnedProofPart {
             Partial::Absent => OwnedProofPart::Absent,
             Partial::Blinded(hash) => OwnedProofPart::Present(MerkleProof::leaf_blind(hash)),
             Partial::Present(data) => OwnedProofPart::Present(MerkleProof::leaf_read(f(data))),
-        }
-    }
-
-    /// Obtain an [`OwnedProofPart`] from a [`Partial<Vec<MerkleProof>>`] considering it a node.
-    pub fn node_from_partial(partial: Partial<Vec<MerkleProof>>) -> Self {
-        match partial {
-            Partial::Absent => OwnedProofPart::Absent,
-            Partial::Blinded(hash) => OwnedProofPart::Present(MerkleProof::leaf_blind(hash)),
-            Partial::Present(children) => OwnedProofPart::Present(MerkleProof::Node(children)),
         }
     }
 
@@ -279,53 +137,13 @@ impl OwnedProofPart {
     }
 }
 
-/// Attempt to compute the partial hash of a node from its children's partial
-/// hashes if they are present. If none of the children hashes can be computed
-/// due to absent data, this node is either a blinded leaf in the proof, in which
-/// case its hash can be recovered from the proof, or it is part of a blinded
-/// subtree whose hash cannot be computed as this point.
-pub fn combine_partial_hashes(
-    hash_results: impl AsRef<[Result<Hash, PartialHashError>]>,
-    proof_hash: Option<Hash>,
-) -> Result<Hash, PartialHashError> {
-    let hash_results = hash_results.as_ref();
-    if hash_results.is_empty() {
-        return Ok(Hash::combine::<Hash, _>([]));
-    }
-
-    // If the first result is a hash, all results need to be a hash in order to
-    // compute the combined hash. If the first result is a potentially
-    // recoverable error, all results need to to be potentially recoverable
-    // errors in order to fall back on the proof hash. Anything else is a fatal error.
-    let expect_ok = match hash_results[0] {
-        Ok(_) => true,
-        Err(PartialHashError::PotentiallyRecoverable) => false,
-        _ => return Err(PartialHashError::Fatal),
-    };
-
-    let mut hashes = Vec::with_capacity(hash_results.len());
-    let hash_results_len = hash_results.len();
-    for r in hash_results {
-        match r {
-            Ok(hash) if expect_ok => hashes.push(*hash),
-            Err(PartialHashError::PotentiallyRecoverable) if !expect_ok => (),
-            _ => return Err(PartialHashError::Fatal),
-        }
-    }
-
-    if expect_ok {
-        debug_assert_eq!(hashes.len(), hash_results_len);
-        return Ok(Hash::combine(hashes));
-    };
-
-    proof_hash.ok_or(PartialHashError::PotentiallyRecoverable)
-}
-
 #[cfg(test)]
 mod tests {
+    use octez_riscv_data::hash::Hash;
     use octez_riscv_data::hash::PartialHash;
     use octez_riscv_data::merkle_tree::MerkleTree;
     use octez_riscv_data::mode::Prove;
+    use octez_riscv_data::mode::Verify;
     use proptest::prop_assert;
     use proptest::prop_assert_eq;
     use proptest::proptest;
