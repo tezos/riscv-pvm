f124ca664ef5c6d4ca0c578fe3bad57c983c67a3
Ole Krüger <ole.kruger@trili.tech>
2025-12-17 17:29:52 +0200

    refactor(projection): mark projection methods as inlineable

--- src/riscv/lib/src/state_context/projection.rs
+++ src/riscv/lib/src/state_context/projection.rs
@@ -249,6 +249,7 @@ impl<P: Projection> Projection for BoxProj<P> {
 
     type Parameter = P::Parameter;
 
+    #[inline]
     fn project_ref<'a, MC: MemoryConfig, M: ManagerRead + 'a>(
         state: &'a ApplyCons<Self::Subject, MC, M>,
         param: Self::Parameter,
@@ -256,6 +257,7 @@ impl<P: Projection> Projection for BoxProj<P> {
         P::project_ref::<MC, M>(state.deref(), param)
     }
 
+    #[inline]
     fn project_read<'a, MC: MemoryConfig, M: ManagerRead + 'a>(
         state: &'a ApplyCons<Self::Subject, MC, M>,
         param: Self::Parameter,
@@ -266,6 +268,7 @@ impl<P: Projection> Projection for BoxProj<P> {
         P::project_read::<MC, M>(state.deref(), param)
     }
 
+    #[inline]
     fn project_write<'a, MC: MemoryConfig, M: ManagerWrite + 'a>(
         state: &'a mut ApplyCons<Self::Subject, MC, M>,
         param: Self::Parameter,
@@ -302,6 +305,7 @@ impl<P: Projection, const LEN: usize> Projection for ArrayProj<P, LEN> {
 
     type Parameter = ArrayProjParam<P::Parameter>;
 
+    #[inline]
     fn project_ref<'a, MC: MemoryConfig, M: ManagerRead + 'a>(
         state: &'a ApplyCons<Self::Subject, MC, M>,
         param: Self::Parameter,
@@ -310,6 +314,7 @@ impl<P: Projection, const LEN: usize> Projection for ArrayProj<P, LEN> {
         P::project_ref::<MC, M>(inner_state, param.inner_param)
     }
 
+    #[inline]
     fn project_read<'a, MC: MemoryConfig, M: ManagerRead + 'a>(
         state: &'a ApplyCons<Self::Subject, MC, M>,
         param: Self::Parameter,
@@ -321,6 +326,7 @@ impl<P: Projection, const LEN: usize> Projection for ArrayProj<P, LEN> {
         P::project_read::<MC, M>(inner_state, param.inner_param)
     }
 
+    #[inline]
     fn project_write<'a, MC: MemoryConfig, M: ManagerWrite + 'a>(
         state: &'a mut ApplyCons<Self::Subject, MC, M>,
         param: Self::Parameter,
