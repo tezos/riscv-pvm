4c762d52955dc4a0b5572e21c8d5eaeb1c791c6f
Ole Krüger <ole.kruger@trili.tech>
2026-01-05 16:18:52 +0000

    test(state): partial Elem writes don't behave correctly

--- src/riscv/lib/src/state_backend.rs
+++ src/riscv/lib/src/state_backend.rs
@@ -246,3 +246,95 @@ pub(crate) mod test_helpers {
             ;
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::num::NonZeroUsize;
+
+    use octez_riscv_data::hash::Hash;
+    use octez_riscv_data::hash::PartialHash;
+    use octez_riscv_data::merkle_proof::FromProof;
+    use octez_riscv_data::merkle_tree::MerkleTree;
+    use rand::RngCore;
+
+    use super::*;
+    use crate::state_backend::proof_backend::merkle::merkle_tree_to_merkle_proof;
+    use crate::state_backend::proof_backend::proof::deserialise_owned::ProofTreeDeserialiser;
+
+    /// Data structure whose [`Elem`] implementation only writes to part of the given space
+    #[derive(Clone)]
+    struct PartialWrite {
+        a: u32,
+        b: u64,
+    }
+
+    impl Elem for PartialWrite {
+        const STORED_SIZE: NonZeroUsize = NonZeroUsize::new(16).unwrap();
+
+        unsafe fn write_unaligned(self, dest: *mut u8) {
+            unsafe {
+                dest.cast::<u32>().write(self.a);
+                dest.add(8).cast::<u64>().write(self.b);
+            }
+        }
+
+        unsafe fn read_unaligned(source: *const u8) -> Self {
+            unsafe {
+                let a = source.cast::<u32>().read();
+                let b = source.add(8).cast::<u64>().read();
+                PartialWrite { a, b }
+            }
+        }
+    }
+
+    #[test]
+    #[ignore = "Exposes a bug which is not yet fixed (RV-854)"]
+    fn test_partial_elem_impls() {
+        const LEN: usize = 4096;
+
+        let mut mem_normal = DynCells::new(LEN);
+
+        // Randomise the contents
+        let mut rand_buffer = [0u8; LEN];
+        rand::rng().fill_bytes(&mut rand_buffer);
+        mem_normal.write_all(0, &rand_buffer);
+
+        // State in Prove mode needs to be derived from the Normal mode state
+        let mem_prove_source = mem_normal.clone();
+        let mut mem_prove = mem_prove_source.start_proof();
+
+        // This is the value that we want to write in all modes
+        let value = PartialWrite {
+            a: 0xDEADBEEF,
+            b: 0x1122334455667788,
+        };
+
+        unsafe {
+            // Perform the writes in Normal and Prove mode to start out, Verify will follow
+            mem_normal.write(0, value.clone());
+            mem_prove.write(0, value.clone());
+        }
+
+        // The Verify mode needs a proof, so we generate it from the Prove mode
+        let merkle_tree = MerkleTree::from_foldable(&mem_prove);
+        let proof_tree = merkle_tree_to_merkle_proof(merkle_tree);
+        let proof_deser = ProofTreeDeserialiser::from(ProofTree::Present(&proof_tree));
+        let mut mem_verify = DynCells::from_proof(proof_deser).unwrap().into_result();
+
+        unsafe {
+            // Finally, also perform the write in Verify mode
+            mem_verify.write(0, value);
+        }
+
+        // Normal and Prove mode should be identical
+        let hash_normal = Hash::from_foldable(&mem_normal);
+        let hash_prove = Hash::from_foldable(&mem_prove);
+        assert_eq!(hash_normal, hash_prove);
+
+        // Normal and Verify mode should be identical
+        let hash_verify = PartialHash::from_foldable(Some(&proof_tree), &mem_verify)
+            .to_hash()
+            .unwrap();
+        assert_eq!(hash_normal, hash_verify);
+    }
+}
