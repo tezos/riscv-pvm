620d0d8f61bc57877b8eafa60c677b53547f0a27
Emma Turner <emma.turner@trili.tech>
2026-01-16 17:48:20 +0000

    fix(merkle_layer): error on Key decoding if slice too large

--- durable-storage/src/merkle_layer.rs
+++ durable-storage/src/merkle_layer.rs
@@ -1,11 +1,6 @@
-// SPDX-FileCopyrightText: 2025 Trilitech <contact@trili.tech>
+// SPDX-FileCopyrightText: 2025-2026 Trilitech <contact@trili.tech>
 //
 // SPDX-License-Identifier: MIT
-
-use std::sync::Arc;
-
-use bytes::Bytes;
-use bytes::BytesMut;
 
 mod node;
 #[cfg(not(feature = "bench"))]
@@ -13,8 +8,14 @@ mod tree;
 #[cfg(feature = "bench")]
 pub mod tree;
 
+use std::sync::Arc;
+
+use bincode::BorrowDecode;
 use bincode::Decode;
 use bincode::Encode;
+use bincode::de::BorrowDecoder;
+use bytes::Bytes;
+use bytes::BytesMut;
 use octez_riscv_data::hash::Hash;
 use tree::Avl;
 
@@ -33,21 +34,54 @@ impl CommitId {
 }
 
 /// A unique key used to store, retrieve and mutate data in durable storage.
-#[derive(Clone, Debug, Decode, Default, Encode, Eq, Hash, Ord, PartialEq, PartialOrd)]
+#[derive(Clone, Debug, Default, Encode, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Key(Vec<u8>);
 
 impl Key {
     pub fn new(bytes: &[u8]) -> Result<Self, MerkleLayerError> {
+        Self::check_bytes_validity(bytes)?;
+
+        Ok(Key(bytes.to_vec()))
+    }
+
+    /// Check whether a given byte-slice is a valid key.
+    fn check_bytes_validity(bytes: &[u8]) -> Result<(), MerkleLayerError> {
         if bytes.len() > KEY_MAX_SIZE {
             return Err(MerkleLayerError::KeyTooLong);
         }
-        Ok(Key(bytes.to_vec()))
+
+        Ok(())
     }
 }
 
 impl AsRef<[u8]> for Key {
     fn as_ref(&self) -> &[u8] {
         &self.0
+    }
+}
+
+// Manual implementation of Decode to ensure that we do not decode a key that is formed of
+// invalid bytes.
+impl<Context> Decode<Context> for Key {
+    fn decode<D: bincode::de::Decoder<Context = Context>>(
+        decoder: &mut D,
+    ) -> Result<Self, bincode::error::DecodeError> {
+        let bytes = Vec::decode(decoder)?;
+
+        Self::check_bytes_validity(&bytes)
+            .map_err(|err| bincode::error::DecodeError::OtherString(err.to_string()))?;
+
+        Ok(Key(bytes))
+    }
+}
+
+// Manual implementation of BorrowDecode to ensure that we do not decode a key that would
+// be longer than `KEY_MAX_SIZE`.
+impl<'de, Context> BorrowDecode<'de, Context> for Key {
+    fn borrow_decode<D: BorrowDecoder<'de, Context = Context>>(
+        decoder: &mut D,
+    ) -> Result<Self, bincode::error::DecodeError> {
+        Key::decode(decoder)
     }
 }
 
@@ -1009,5 +1043,59 @@ mod tests {
 
         let root_hash = merkle_layer.tree.hash();
         assert_eq!(commit_id.0, root_hash);
+    }
+
+    proptest! {
+        #[test]
+        fn key_encode_decode_ok(key_len in 0..=KEY_MAX_SIZE) {
+            use octez_riscv_data::serialisation as binary;
+
+            let bytes = vec![0; key_len];
+            let key = Key::new(&bytes).expect("Key is valid");
+
+            let serialised = binary::serialise(&key)
+                .expect("serialisation of a key should succeed");
+
+            let key_decoded = binary::deserialise(&serialised)
+                .expect("Decoding of a valid encoded key should succeed");
+
+            assert_eq!(key, key_decoded, "encode then decode must produce the same key");
+
+            // TODO: RV-838: introduce and use a direct helper from `octez_riscv_data::deserialisation`
+            //               for borrow-decode
+            let (key_decoded_slice, _): (Key, _) = bincode::borrow_decode_from_slice(
+                &serialised,
+                binary::bincode_default_config(),
+            )
+                .expect("Borrow decoding of a valid encoded key should succeed");
+
+            assert_eq!(key, key_decoded_slice, "encode then borrow decode must produce the same key");
+        }
+    }
+
+    #[test]
+    fn key_decode_protects_key_too_large() {
+        // NB the public api does not allow for such an invalid key.
+        let key = Key(vec![0; KEY_MAX_SIZE + 1]);
+
+        let bytes = octez_riscv_data::serialisation::serialise(&key)
+            .expect("serialisation of a key should succeed");
+
+        let res: Result<Key, _> = octez_riscv_data::serialisation::deserialise(&bytes);
+        assert!(
+            res.is_err(),
+            "Decoding of a key that's too large should fail"
+        );
+
+        // TODO: RV-838: introduce and use a direct helper from `octez_riscv_data::deserialisation`
+        //               for borrow-decode
+        let res: Result<(Key, usize), _> = bincode::borrow_decode_from_slice(
+            &bytes,
+            octez_riscv_data::serialisation::bincode_default_config(),
+        );
+        assert!(
+            res.is_err(),
+            "Decoding of a key that's too large should fail"
+        );
     }
 }
