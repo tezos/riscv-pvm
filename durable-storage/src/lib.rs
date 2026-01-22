7a992ab385bc3f7b062b2a3fa68a0a4bfa2bec48
Ole Krüger <ole.kruger@trili.tech>
2026-01-22 16:30:07 +0000

    style(durable-storage): group pub module declarations together

--- durable-storage/src/lib.rs
+++ durable-storage/src/lib.rs
@@ -26,8 +26,6 @@
 //! - **Persistence layer**: Responsible for actually persisting the basic get, set, delete
 //!   operations on disk.
 
-pub mod key;
-
 cfg_if::cfg_if! {
     if #[cfg(feature = "bench")] {
         pub mod merkle_layer;
@@ -46,4 +44,5 @@ cfg_if::cfg_if! {
 }
 
 pub mod database;
+pub mod key;
 pub mod registry;
