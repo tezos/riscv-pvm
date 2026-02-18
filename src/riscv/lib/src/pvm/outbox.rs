68b9f58e9c76ee40cb43b48406fc0e66e8c158a5
Victor Dumitrescu <victor.dumitrescu@nomadic-labs.com>
2026-02-18 09:49:36 +0100

    refactor(outbox): split outbox message types into separate module

--- src/riscv/lib/src/pvm/outbox.rs
+++ src/riscv/lib/src/pvm/outbox.rs
@@ -15,8 +15,8 @@
 //! - Producing an outbox proof for a given message
 //! - Verifying an outbox proof
 
-use std::ops::Deref;
-use std::ops::DerefMut;
+mod output;
+
 use std::ops::Index;
 
 use bincode::Decode;
@@ -49,10 +49,13 @@ use octez_riscv_data::mode::Prove;
 use octez_riscv_data::mode::Verify;
 use octez_riscv_data::serialisation::serialise;
 use perfect_derive::perfect_derive;
-use tezos_smart_rollup_constants::core::MAX_OUTPUT_SIZE;
 use tezos_smart_rollup_constants::riscv::SbiError;
 use thiserror::Error;
 
+pub use self::output::OutboxMessage;
+pub use self::output::OutboxMessageError;
+pub use self::output::Output;
+pub use self::output::OutputInfo;
 use super::Pvm;
 use super::durable_storage::DurableStorage;
 use crate::machine_state::memory::MemoryConfig;
@@ -74,33 +77,6 @@ const OUTBOX_MERKLE_ARITY: usize = 2;
 /// The arity used to Merkleise arrays in each level
 const LEVEL_MERKLE_ARITY: usize = 2;
 
-/// The outbox level and the index within that level for an outbox message
-#[derive(Debug, PartialEq, Eq, Copy, Clone, Encode)]
-pub struct OutputInfo {
-    pub level: u32,
-    pub index: u32,
-}
-
-/// A raw outbox message and its outbox information
-#[derive(Debug, PartialEq, Eq)]
-pub struct Output {
-    pub message: OutboxMessage,
-    pub info: OutputInfo,
-}
-
-/// Errors which can be raised when producing or verifying an outbox proof
-#[derive(Error, Debug, PartialEq, Eq)]
-pub enum OutboxProofError {
-    #[error("The outbox does not contain the level {level}")]
-    LevelNotFound { level: u32 },
-
-    #[error("The outbox for level {} does not contain a message at index {}", info.level, info.index)]
-    MessageNotFound { info: OutputInfo },
-
-    #[error(transparent)]
-    MessageError(#[from] OutboxMessageError),
-}
-
 /// Errors which can be raised when writing a message to the outbox
 #[derive(Error, Debug)]
 pub(crate) enum OutboxWriteError {
@@ -118,6 +94,19 @@ impl From<OutboxWriteError> for SbiError {
             OutboxWriteError::MessageError(e) => e.into(),
         }
     }
+}
+
+/// Errors which can be raised when producing or verifying an outbox proof
+#[derive(Error, Debug, PartialEq, Eq)]
+pub enum OutboxProofError {
+    #[error("The outbox does not contain the level {level}")]
+    LevelNotFound { level: u32 },
+
+    #[error("The outbox for level {} does not contain a message at index {}", info.level, info.index)]
+    MessageNotFound { info: OutputInfo },
+
+    #[error(transparent)]
+    MessageError(#[from] OutboxMessageError),
 }
 
 /// An outbox proof, containing a partial Merkle tree of a PVM state which ties
@@ -339,7 +328,7 @@ impl<M: AtomMode> OutboxLevel<M> {
             return Err(OutboxWriteError::FullOutbox);
         }
 
-        self.messages[next_index].write(message.0);
+        self.messages[next_index].write(message.into());
         self.next_index.write(next_index as u32 + 1);
 
         Ok(())
@@ -478,72 +467,6 @@ impl<C> Decode<C> for OutboxLevel<Normal> {
     }
 }
 
-#[derive(Error, Debug, PartialEq, Eq)]
-pub enum OutboxMessageError {
-    #[error(
-        "The size of the outbox message is {size} B, which is larger than the maximum message size ({MAX_OUTPUT_SIZE})."
-    )]
-    MessageTooLarge { size: usize },
-}
-
-impl From<OutboxMessageError> for SbiError {
-    fn from(err: OutboxMessageError) -> Self {
-        match err {
-            OutboxMessageError::MessageTooLarge { .. } => Self::OutputTooLarge,
-        }
-    }
-}
-
-/// An Outbox Message is a boxed byte slice, restricted to at most [`MAX_OUTPUT_SIZE`]
-/// in length
-#[derive(Clone, Debug, PartialEq, Eq)]
-#[repr(transparent)]
-pub struct OutboxMessage(Box<[u8]>);
-
-impl OutboxMessage {
-    /// Constructs a zeroed, boxed outbox message buffer of size `size`
-    ///
-    /// Fails if `size` exceeds [`MAX_OUTPUT_SIZE`]
-    pub(crate) fn new(size: usize) -> Result<Self, OutboxMessageError> {
-        if size > MAX_OUTPUT_SIZE {
-            return Err(OutboxMessageError::MessageTooLarge { size });
-        }
-        let boxed_slice = vec![0u8; size].into_boxed_slice();
-        Ok(OutboxMessage(boxed_slice))
-    }
-}
-
-impl TryFrom<Box<[u8]>> for OutboxMessage {
-    type Error = OutboxMessageError;
-
-    fn try_from(value: Box<[u8]>) -> Result<Self, Self::Error> {
-        if value.len() > MAX_OUTPUT_SIZE {
-            return Err(OutboxMessageError::MessageTooLarge { size: value.len() });
-        }
-        Ok(OutboxMessage(value))
-    }
-}
-
-impl From<OutboxMessage> for Box<[u8]> {
-    fn from(message: OutboxMessage) -> Self {
-        message.0
-    }
-}
-
-impl Deref for OutboxMessage {
-    type Target = [u8];
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl DerefMut for OutboxMessage {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use std::ops::Bound::*;
@@ -552,6 +475,7 @@ mod tests {
 
     use itertools::Itertools;
     use proptest::prelude::*;
+    use tezos_smart_rollup_constants::core::MAX_OUTPUT_SIZE;
 
     use super::*;
     use crate::machine_state::memory::M1M;
@@ -577,7 +501,7 @@ mod tests {
     ) -> impl Strategy<Value = OutboxMessage> {
         let safe_range = safe_size_range(size_range);
         proptest::collection::vec(any::<u8>(), safe_range)
-            .prop_map(|data| OutboxMessage(data.into_boxed_slice()))
+            .prop_map(|data| OutboxMessage::try_from(data.into_boxed_slice()).unwrap())
     }
 
     fn messages_strategy(
@@ -585,20 +509,6 @@ mod tests {
         len: usize,
     ) -> impl Strategy<Value = Vec<OutboxMessage>> {
         proptest::collection::vec(message_strategy(size_range), len)
-    }
-
-    #[test]
-    fn test_outbox_message_too_large() {
-        let size = MAX_OUTPUT_SIZE + 1;
-        assert_eq!(
-            OutboxMessage::try_from(vec![1u8; size].into_boxed_slice()),
-            Err(OutboxMessageError::MessageTooLarge { size })
-        );
-
-        proptest!(|(size in MAX_OUTPUT_SIZE + 1..)| {
-            let res = OutboxMessage::new(size).unwrap_err();
-            assert!(matches!(res.into(), SbiError::OutputTooLarge));
-        })
     }
 
     #[test]
@@ -612,7 +522,7 @@ mod tests {
             for (i, message) in messages.into_iter().enumerate() {
                 prop_assert!(outbox.write_message(message.clone(), level).is_ok());
 
-                prop_assert_eq!(&*outbox.levels[idx].messages[i], &message.0);
+                prop_assert_eq!(&*outbox.levels[idx].messages[i], &message.into());
                 prop_assert_eq!(*outbox.levels[idx].next_index, (i + 1) as u32);
                 prop_assert_eq!(*outbox.levels[idx].level, level);
             }
@@ -623,8 +533,8 @@ mod tests {
     fn write_message_level_wrap() {
         proptest!(|(level in 0u32..TEST_OUTBOX_SIZE as u32)| {
             let mut outbox = Outbox::<Normal>::default();
-            let first_message = OutboxMessage(Box::new([1u8; 32]));
-            let second_message = OutboxMessage(Box::new([2u8; 32]));
+            let first_message = OutboxMessage::try_from(vec![1u8; 32].into_boxed_slice()).unwrap();
+            let second_message = OutboxMessage::try_from(vec![2u8; 32].into_boxed_slice()).unwrap();
             let idx = level as usize;
 
             // Write two messages at `level`
@@ -636,10 +546,10 @@ mod tests {
 
             // Now write at `level` + TEST_OUTBOX_SIZE which should wrap to `level`
             let overflow_level = level + TEST_OUTBOX_SIZE as u32;
-            let third_message = OutboxMessage(Box::new([3u8; 32]));
+            let third_message = OutboxMessage::try_from(vec![3u8; 32].into_boxed_slice()).unwrap();
             assert!(outbox.write_message(third_message.clone(), overflow_level).is_ok());
 
-            assert_eq!(&*outbox.levels[idx].messages[0], &third_message.0);
+            assert_eq!(&*outbox.levels[idx].messages[0], third_message.as_ref());
             assert_eq!(*outbox.levels[idx].next_index, 1);
             assert_eq!(*outbox.levels[idx].level, overflow_level);
         });
@@ -652,14 +562,17 @@ mod tests {
              for (i, message) in messages.iter().enumerate().take(MAX_LEVEL_SIZE) {
                 assert!(outbox.write_message(message.clone(), 0).is_ok());
                 assert_eq!(*outbox.levels[0].next_index, (i + 1) as u32);
-                assert_eq!(&*outbox.levels[0].messages[i], &message.0);
+                assert_eq!(&*outbox.levels[0].messages[i], message.as_ref());
             }
 
             for message in &messages[MAX_LEVEL_SIZE..] {
                 // Verify that outbox is full
                 assert_eq!(*outbox.levels[0].next_index, MAX_LEVEL_SIZE as u32);
 
-                prop_assert!(matches!(outbox.write_message(message.clone(), 0), Err(OutboxWriteError::FullOutbox)));
+                prop_assert!(matches!(
+                    outbox.write_message(message.clone(), 0),
+                    Err(OutboxWriteError::FullOutbox)
+                ));
                 prop_assert_eq!(*outbox.levels[0].next_index, MAX_LEVEL_SIZE as u32);
             }
         });
@@ -704,7 +617,13 @@ mod tests {
                     prop_assert!(outbox.write_message(msg.clone(), wrap_level as u32).is_ok());
                 }
                 let read_messages = outbox.read_level(wrap_level as u32);
-                let expected_messages: Box<[Box<[u8]>]> = Box::from(messages2[offset].clone().into_iter().map(|m|m.0).collect_vec());
+                let expected_messages: Box<[Box<[u8]>]> = Box::from(
+                    messages2[offset]
+                        .clone()
+                        .into_iter()
+                        .map(|m| m.into())
+                        .collect_vec(),
+                );
                 prop_assert_eq!(read_messages, expected_messages);
             }
         });
@@ -739,7 +658,7 @@ mod tests {
                     index: i as u32,
                 };
                 let output = outbox.read_message(info).unwrap();
-                prop_assert_eq!(&*output.message, &*message.0);
+                prop_assert_eq!(&output.message, message);
                 prop_assert_eq!(output.info, info);
             }
         });
