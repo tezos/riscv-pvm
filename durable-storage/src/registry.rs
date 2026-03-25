8e2a259266d803454f1f5521a43141ded715d0e4
Ole Krüger <ole.kruger@trili.tech>
2026-03-25 23:04:00 +0000

    feat(durable-storage): use vector component for registry

--- durable-storage/src/registry.rs
+++ durable-storage/src/registry.rs
@@ -13,6 +13,8 @@ use std::sync::Arc;
 
 use bincode::Decode;
 use bincode::Encode;
+use octez_riscv_data::components::vector::Vector;
+use octez_riscv_data::components::vector::VectorMode;
 use octez_riscv_data::foldable::Fold;
 use octez_riscv_data::foldable::Foldable;
 use octez_riscv_data::foldable::seq_tree::IndexableSeqAsTree;
@@ -43,9 +45,9 @@ struct RegistryManifest {
 }
 
 /// Registry that owns a set of databases backed by a directory manager.
-#[repr(transparent)]
 pub struct Registry<KV: KeyValueStore, M: Mode> {
     inner: M::Select<RegistryTemplate<KV>>,
+    databases: Vector<Database<KV, M>, M>,
 }
 
 impl<KV: BackgroundKeyValueStore> Registry<KV, Normal> {
@@ -57,11 +59,8 @@ impl<KV: BackgroundKeyValueStore> Registry<KV, Normal> {
         let runtime = Self::build_runtime()?;
 
         Ok(Registry {
-            inner: NormalImpl {
-                repo,
-                databases: Vec::new(),
-                runtime,
-            },
+            inner: NormalImpl { repo, runtime },
+            databases: Vector::new(Vec::new()),
         })
     }
 
@@ -86,11 +85,8 @@ impl<KV: BackgroundPersistentKeyValueStore<Repo = DirectoryManager>> Registry<KV
         let databases = Self::checkout_databases(&runtime, &repo, &manifest.database_hashes)?;
 
         let registry = Registry {
-            inner: NormalImpl {
-                repo,
-                databases,
-                runtime,
-            },
+            inner: NormalImpl { repo, runtime },
+            databases: Vector::new(databases),
         };
 
         let actual_commit = CommitId::from(Hash::from_foldable(&registry));
@@ -135,9 +131,9 @@ impl<KV: BackgroundPersistentKeyValueStore<Repo = DirectoryManager>> Registry<KV
     /// of all underlying databases through the [`Foldable<HashFold>`] implementation,
     /// and the registry manifest is stored at the corresponding commit path.
     pub fn commit(&self) -> Result<CommitId, OperationalError> {
-        let mut database_hashes = Vec::with_capacity(self.inner.databases.len());
+        let mut database_hashes = Vec::with_capacity(self.databases.len());
 
-        for database in &self.inner.databases {
+        for database in self.databases.iter() {
             let hash = database.commit(&self.inner.repo)?;
             database_hashes.push(hash);
         }
@@ -156,15 +152,33 @@ impl<KV: BackgroundPersistentKeyValueStore<Repo = DirectoryManager>> Registry<KV
     }
 }
 
-impl<KV: KeyValueStore, M: RegistryMode> Registry<KV, M> {
+impl<KV: KeyValueStore, M: Mode> Registry<KV, M> {
+    /// Check the given `index` is valid for a database in the registry.
+    fn validate_index(&self, index: usize) -> Result<(), InvalidArgumentError>
+    where
+        M: VectorMode,
+    {
+        if index >= self.databases.len() {
+            Err(InvalidArgumentError::DatabaseIndexOutOfBounds)
+        } else {
+            Ok(())
+        }
+    }
+
     /// Are there no databases in the registry?
-    pub fn is_empty(&self) -> bool {
+    pub fn is_empty(&self) -> bool
+    where
+        M: VectorMode,
+    {
         self.len() == 0
     }
 
     /// Get the number of databases held in the registry.
-    pub fn len(&self) -> usize {
-        M::len(self)
+    pub fn len(&self) -> usize
+    where
+        M: VectorMode,
+    {
+        self.databases.len()
     }
 
     /// Resize the registry to the given `new_size`.
@@ -177,6 +191,7 @@ impl<KV: KeyValueStore, M: RegistryMode> Registry<KV, M> {
     pub fn resize_tick(&mut self, new_size: usize) -> Result<(), Error>
     where
         KV: BackgroundKeyValueStore,
+        M: RegistryMode + VectorMode,
     {
         match self.len().abs_diff(new_size) {
             1 => (),
@@ -184,25 +199,48 @@ impl<KV: KeyValueStore, M: RegistryMode> Registry<KV, M> {
             _ => return Err(Error::from(InvalidArgumentError::RegistryResizeTooLarge)),
         }
 
-        M::resize(self, new_size)
+        self.databases
+            .try_resize_with(new_size, || M::try_new_database(&self.inner))?;
+
+        Ok(())
     }
 
     /// Get a reference to the database at the given `index`.
-    pub fn database(&self, index: usize) -> Result<&Database<KV, M>, Error> {
-        M::database(self, index)
+    pub fn database(&self, index: usize) -> Result<&Database<KV, M>, Error>
+    where
+        M: VectorMode,
+    {
+        self.validate_index(index)?;
+        Ok(&self.databases[index])
     }
 
     /// Get a mutable reference to the database at the given `index`.
-    pub fn database_mut(&mut self, index: usize) -> Result<&mut Database<KV, M>, Error> {
-        M::database_mut(self, index)
+    pub fn database_mut(&mut self, index: usize) -> Result<&mut Database<KV, M>, Error>
+    where
+        M: VectorMode,
+    {
+        self.validate_index(index)?;
+        Ok(&mut self.databases[index])
     }
 
     /// Copy the contents of database at `src_index` to database at `dst_index`.
     pub fn copy_database(&mut self, src_index: usize, dst_index: usize) -> Result<(), Error>
     where
         KV: BackgroundKeyValueStore,
+        M: RegistryMode + VectorMode,
     {
-        M::copy_database(self, src_index, dst_index)
+        self.validate_index(src_index)?;
+        self.validate_index(dst_index)?;
+
+        if src_index == dst_index {
+            // No-op if copying to the same index.
+            return Ok(());
+        }
+
+        let db_copy = M::try_clone_database(&self.inner, &self.databases[src_index])?;
+        self.databases[dst_index] = db_copy;
+
+        Ok(())
     }
 
     /// Move the contents of database at `src_index` to database at `dst_index`. The source
@@ -210,16 +248,32 @@ impl<KV: KeyValueStore, M: RegistryMode> Registry<KV, M> {
     pub fn move_database(&mut self, src_index: usize, dst_index: usize) -> Result<(), Error>
     where
         KV: BackgroundKeyValueStore,
+        M: RegistryMode + VectorMode,
     {
-        M::move_database(self, src_index, dst_index)
+        self.validate_index(src_index)?;
+        self.validate_index(dst_index)?;
+
+        if src_index == dst_index {
+            // No-op if moving to the same index.
+            return Ok(());
+        }
+
+        let empty = M::try_new_database(&self.inner)?;
+        let db_to_move = std::mem::replace(&mut self.databases[src_index], empty);
+        self.databases[dst_index] = db_to_move;
+
+        Ok(())
     }
 
     /// Clear the database at the given `index`.
     pub fn clear_database(&mut self, index: usize) -> Result<(), Error>
     where
         KV: BackgroundKeyValueStore,
+        M: RegistryMode + VectorMode,
     {
-        M::clear_database(self, index)
+        self.validate_index(index)?;
+        self.databases[index] = M::try_new_database(&self.inner)?;
+        Ok(())
     }
 }
 
@@ -263,140 +317,39 @@ impl<KV: KeyValueStore> Modal for RegistryTemplate<KV> {
 }
 
 /// Modes that implement this support operations on [`Registry`]
+#[expect(
+    private_interfaces,
+    reason = "This method should not be used outside of this module"
+)]
 pub trait RegistryMode: Mode {
-    /// See [`Registry::len`]
-    fn len<KV: KeyValueStore>(this: &Registry<KV, Self>) -> usize;
+    /// Create a new database.
+    fn try_new_database<KV: BackgroundKeyValueStore>(
+        inner: &Self::Select<RegistryTemplate<KV>>,
+    ) -> Result<Database<KV, Self>, OperationalError>;
 
-    /// See [`Registry::resize`]
-    fn resize<KV: BackgroundKeyValueStore>(
-        this: &mut Registry<KV, Self>,
-        new_size: usize,
-    ) -> Result<(), Error>;
-
-    /// See [`Registry::database`]
-    fn database<KV: KeyValueStore>(
-        this: &Registry<KV, Self>,
-        index: usize,
-    ) -> Result<&Database<KV, Self>, Error>;
-
-    /// See [`Registry::database_mut`]
-    fn database_mut<KV: KeyValueStore>(
-        this: &mut Registry<KV, Self>,
-        index: usize,
-    ) -> Result<&mut Database<KV, Self>, Error>;
-
-    /// See [`Registry::copy_database`]
-    fn copy_database<KV: BackgroundKeyValueStore>(
-        this: &mut Registry<KV, Self>,
-        src: usize,
-        dst: usize,
-    ) -> Result<(), Error>;
-
-    /// See [`Registry::move_database`]
-    fn move_database<KV: BackgroundKeyValueStore>(
-        this: &mut Registry<KV, Self>,
-        src: usize,
-        dst: usize,
-    ) -> Result<(), Error>;
-
-    /// See [`Registry::clear_database`]
-    fn clear_database<KV: BackgroundKeyValueStore>(
-        this: &mut Registry<KV, Self>,
-        index: usize,
-    ) -> Result<(), Error>;
+    /// Clone a database
+    fn try_clone_database<KV: BackgroundKeyValueStore>(
+        inner: &Self::Select<RegistryTemplate<KV>>,
+        database: &Database<KV, Self>,
+    ) -> Result<Database<KV, Self>, OperationalError>;
 }
 
+#[expect(
+    private_interfaces,
+    reason = "This method should not be used outside of this module"
+)]
 impl RegistryMode for Normal {
-    fn len<KV: KeyValueStore>(this: &Registry<KV, Self>) -> usize {
-        this.inner.databases.len()
+    fn try_new_database<KV: BackgroundKeyValueStore>(
+        inner: &Self::Select<RegistryTemplate<KV>>,
+    ) -> Result<Database<KV, Self>, OperationalError> {
+        Database::try_new(inner.runtime.handle(), &inner.repo)
     }
 
-    fn resize<KV: BackgroundKeyValueStore>(
-        this: &mut Registry<KV, Self>,
-        new_size: usize,
-    ) -> Result<(), Error> {
-        while this.len() < new_size {
-            let database = Database::try_new(this.inner.runtime.handle(), &this.inner.repo)?;
-            this.inner.databases.push(database);
-        }
-
-        this.inner.databases.truncate(new_size);
-
-        Ok(())
-    }
-
-    fn database<KV: KeyValueStore>(
-        this: &Registry<KV, Self>,
-        index: usize,
-    ) -> Result<&Database<KV, Self>, Error> {
-        let database = this
-            .inner
-            .databases
-            .get(index)
-            .ok_or(InvalidArgumentError::DatabaseIndexOutOfBounds)?;
-        Ok(database)
-    }
-
-    fn database_mut<KV: KeyValueStore>(
-        this: &mut Registry<KV, Self>,
-        index: usize,
-    ) -> Result<&mut Database<KV, Self>, Error> {
-        let database = this
-            .inner
-            .databases
-            .get_mut(index)
-            .ok_or(InvalidArgumentError::DatabaseIndexOutOfBounds)?;
-        Ok(database)
-    }
-
-    fn copy_database<KV: BackgroundKeyValueStore>(
-        this: &mut Registry<KV, Self>,
-        src_index: usize,
-        dst_index: usize,
-    ) -> Result<(), Error> {
-        this.inner.validate_index(src_index)?;
-        this.inner.validate_index(dst_index)?;
-
-        if src_index == dst_index {
-            // No-op if copying to the same index.
-            return Ok(());
-        }
-
-        let db_copy = this.inner.databases[src_index]
-            .try_clone_with(this.inner.runtime.handle(), &this.inner.repo)?;
-        this.inner.databases[dst_index] = db_copy;
-
-        Ok(())
-    }
-
-    fn move_database<KV: BackgroundKeyValueStore>(
-        this: &mut Registry<KV, Self>,
-        src_index: usize,
-        dst_index: usize,
-    ) -> Result<(), Error> {
-        this.inner.validate_index(src_index)?;
-        this.inner.validate_index(dst_index)?;
-
-        if src_index == dst_index {
-            // No-op if moving to the same index.
-            return Ok(());
-        }
-
-        let empty = Database::try_new(this.inner.runtime.handle(), &this.inner.repo)?;
-        let db_to_move = std::mem::replace(&mut this.inner.databases[src_index], empty);
-        this.inner.databases[dst_index] = db_to_move;
-
-        Ok(())
-    }
-
-    fn clear_database<KV: BackgroundKeyValueStore>(
-        this: &mut Registry<KV, Self>,
-        index: usize,
-    ) -> Result<(), Error> {
-        this.inner.validate_index(index)?;
-        this.inner.databases[index] =
-            Database::try_new(this.inner.runtime.handle(), &this.inner.repo)?;
-        Ok(())
+    fn try_clone_database<KV: BackgroundKeyValueStore>(
+        inner: &Self::Select<RegistryTemplate<KV>>,
+        database: &Database<KV, Self>,
+    ) -> Result<Database<KV, Self>, OperationalError> {
+        database.try_clone_with(inner.runtime.handle(), &inner.repo)
     }
 }
 
@@ -421,18 +374,16 @@ impl CloneRegistryMode for Normal {
         let repo = this.inner.repo.clone();
 
         let databases = this
-            .inner
             .databases
             .iter()
             .map(|db| db.try_clone_with(runtime.handle(), &repo))
             .collect::<Result<_, _>>()?;
+        let databases = Vector::new(databases);
 
         Ok(Registry {
-            inner: NormalImpl {
-                repo,
-                databases,
-                runtime,
-            },
+            inner: NormalImpl { repo, runtime },
+
+            databases,
         })
     }
 }
@@ -440,19 +391,7 @@ impl CloneRegistryMode for Normal {
 /// Registry implementation for the [`Normal`] mode
 struct NormalImpl<KV: KeyValueStore> {
     repo: KV::Repo,
-    databases: Vec<Database<KV, Normal>>,
     runtime: Arc<Runtime>,
-}
-
-impl<KV: KeyValueStore> NormalImpl<KV> {
-    /// Check the given `index` is valid for a database in the registry.
-    fn validate_index(&self, index: usize) -> Result<(), InvalidArgumentError> {
-        if index >= self.databases.len() {
-            Err(InvalidArgumentError::DatabaseIndexOutOfBounds)
-        } else {
-            Ok(())
-        }
-    }
 }
 
 #[cfg(test)]
@@ -501,16 +440,16 @@ pub(super) mod tests {
         ];
 
         for (key, value) in src_pairs.iter() {
-            registry.inner.databases[src_index]
+            registry.databases[src_index]
                 .write(key.clone(), 0, Bytes::copy_from_slice(value))
                 .expect("Writing to source should succeed");
         }
 
         // Write values to destination that should be overwritten.
-        registry.inner.databases[dst_index]
+        registry.databases[dst_index]
             .write(key_a, 0, Bytes::copy_from_slice(b"old_a"))
             .expect("Writing to destination should succeed");
-        registry.inner.databases[dst_index]
+        registry.databases[dst_index]
             .write(key_c.clone(), 0, Bytes::copy_from_slice(b"old_c"))
             .expect("Writing to destination should succeed");
 
@@ -524,12 +463,12 @@ pub(super) mod tests {
     ) {
         for (key, value) in pairs.iter() {
             assert!(
-                registry.inner.databases[db_index]
+                registry.databases[db_index]
                     .exists(key)
                     .expect("Checking destination should succeed")
             );
             let mut buf = vec![0u8; value.len()];
-            registry.inner.databases[db_index]
+            registry.databases[db_index]
                 .read(key, 0, buf.as_mut_slice())
                 .expect("Reading from destination should succeed");
             assert_eq!(&buf, value);
@@ -543,7 +482,7 @@ pub(super) mod tests {
     ) {
         for (key, _value) in pairs.iter() {
             assert!(
-                !registry.inner.databases[db_index]
+                !registry.databases[db_index]
                     .exists(key)
                     .expect("Checking source should succeed"),
                 "Key should not exist in source after move."
@@ -558,7 +497,7 @@ pub(super) mod tests {
         value: &[u8],
     ) {
         let key = Key::new(key_bytes).expect("Key should be valid");
-        registry.inner.databases[db_index]
+        registry.databases[db_index]
             .set(key, Bytes::copy_from_slice(value))
             .expect("Writing to database should succeed");
     }
@@ -625,7 +564,7 @@ pub(super) mod tests {
         assert_pairs_present(&registry, dst_index, &src_pairs);
 
         assert!(
-            !registry.inner.databases[dst_index]
+            !registry.databases[dst_index]
                 .exists(&key_c)
                 .expect("Checking destination should succeed"),
             "Key C should not exist in destination after copy."
@@ -722,7 +661,7 @@ pub(super) mod tests {
             .expect("Clearing the database should succeed");
 
         assert!(
-            !registry.inner.databases[db_index]
+            !registry.databases[db_index]
                 .exists(&key)
                 .expect("Checking database should succeed"),
             "Key should not exist after clearing the database."
@@ -790,7 +729,6 @@ mod rocksdb_tests {
         populate_database_with_key_value(&mut registry, 0, &[1], b"singleton");
 
         let expected_db_hashes: Vec<super::CommitId> = registry
-            .inner
             .databases
             .iter()
             .map(|db| db.hash().unwrap().into())
@@ -831,7 +769,6 @@ mod rocksdb_tests {
         populate_database_with_key_value(&mut registry, 1, &[2], b"beta");
 
         let expected_db_hashes: Vec<super::CommitId> = registry
-            .inner
             .databases
             .iter()
             .map(|db| db.hash().unwrap().into())
@@ -878,8 +815,8 @@ mod rocksdb_tests {
             .expect("Checkout should succeed");
 
         assert_eq!(checked_out.len(), 2);
-        checked_out.inner.databases[0].assert_database_value(&key_a, b"alpha");
-        checked_out.inner.databases[1].assert_database_value(&key_b, b"beta");
+        checked_out.databases[0].assert_database_value(&key_a, b"alpha");
+        checked_out.databases[1].assert_database_value(&key_b, b"beta");
         assert_eq!(
             CommitId::from(Hash::from_foldable(&checked_out)),
             root_commit
