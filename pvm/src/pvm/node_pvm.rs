9baed5f73e37e9fba61caa0df4cba80f482f5c8e
Victor Dumitrescu <victor.dumitrescu@nomadic-labs.com>
2026-03-26 15:49:22 +0100

    fix(node_pvm): verification returns input request for correct step

--- pvm/src/pvm/node_pvm.rs
+++ pvm/src/pvm/node_pvm.rs
@@ -262,6 +262,8 @@ impl<DS: DurableStorage<Verify>> NodePvm<Verify, EmptyPageCache, DS> {
         DS: Foldable<PartialHashFold>,
     {
         self.with_backend_mut(|pvm| {
+            let input_request = pvm.input_request();
+
             match input {
                 None => pvm.eval_one(pvm_hooks),
                 Some(input) => {
@@ -277,7 +279,7 @@ impl<DS: DurableStorage<Verify>> NodePvm<Verify, EmptyPageCache, DS> {
                 return None;
             }
 
-            Some(pvm.input_request())
+            Some(input_request)
         })
     }
 }
