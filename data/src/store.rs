149c42c5a7a6d2b94044c55428a913b96ca67c7d
Thomas Athorne <thomas.athorne@trili.tech>
2026-04-09 15:22:01 +0100

    refactor(blob-store): blob store error type

--- data/src/store.rs
+++ data/src/store.rs
@@ -15,32 +15,45 @@ use bytes::Bytes;
 use crate::hash::Hash;
 use crate::hash::HashedData;
 
+/// Error type for errors that can occur in any `BlobStore` implementation, including a `Custom`
+/// variant to allow different implementations to include their own specific errors.
+#[derive(Debug, thiserror::Error)]
+pub enum BlobStoreError {
+    #[error("Blob not found in store: {0}")]
+    NotFound(Hash),
+
+    #[error("Store-specific error: {0}")]
+    Custom(Box<dyn std::error::Error + Send + Sync>),
+}
+
 /// Content addressable store that can contain arbitrary blobs of data indexed by their hashes.
 pub trait BlobStore {
-    type Error: std::error::Error + 'static;
-
-    /// Retrieve a blob from its hash. If a hash is not found that must be somehow represented with
-    /// the `Error` return type.
-    fn blob_get(&self, key: Hash) -> Result<impl AsRef<[u8]>, Self::Error>;
+    /// Retrieve a blob from its hash. Will return `BlobStoreError::NotFound` if the hash is not
+    /// present in the store.
+    fn blob_get(&self, key: Hash) -> Result<impl AsRef<[u8]>, BlobStoreError>;
 
     /// Store a blob under its hash; should be a no-op if it is already present.
-    fn blob_set<Data: AsRef<[u8]>>(&self, blob: &HashedData<Data>) -> Result<(), Self::Error>;
+    fn blob_set<Data: AsRef<[u8]>>(&self, blob: &HashedData<Data>) -> Result<(), BlobStoreError>;
 
     /// Remove an item from the store; should be a no-op if it is already absent.
-    fn blob_delete(&self, key: Hash) -> Result<(), Self::Error>;
+    fn blob_delete(&self, key: Hash) -> Result<(), BlobStoreError>;
 }
 
 /// Basic implementation of the `BlobStore` trait for use in tests and any other time we don't want
 /// to persist data to disk.
 pub struct InMemoryBlobStore(RwLock<HashMap<Hash, Bytes>>);
 
+/// Error type for errors that are specific to the `InMemoryBlobStore`.
 #[derive(Debug, thiserror::Error)]
 pub enum InMemoryError {
-    #[error("Blob not found in store: {0}")]
-    NotFound(Hash),
-
     #[error("RwLock in blob store was poisoned.")]
     LockPoisoned,
+}
+
+impl From<InMemoryError> for BlobStoreError {
+    fn from(e: InMemoryError) -> Self {
+        Self::Custom(Box::new(e))
+    }
 }
 
 #[cfg(test)]
@@ -51,23 +64,21 @@ impl InMemoryBlobStore {
 }
 
 impl BlobStore for InMemoryBlobStore {
-    type Error = InMemoryError;
-
-    fn blob_get(&self, key: Hash) -> Result<impl AsRef<[u8]>, Self::Error> {
+    fn blob_get(&self, key: Hash) -> Result<impl AsRef<[u8]>, BlobStoreError> {
         let store = self.0.read().map_err(|_| InMemoryError::LockPoisoned)?;
         match store.get(&key) {
             Some(blob) => Ok(blob.clone()),
-            None => Err(InMemoryError::NotFound(key)),
+            None => Err(BlobStoreError::NotFound(key)),
         }
     }
 
-    fn blob_set<Data: AsRef<[u8]>>(&self, blob: &HashedData<Data>) -> Result<(), Self::Error> {
+    fn blob_set<Data: AsRef<[u8]>>(&self, blob: &HashedData<Data>) -> Result<(), BlobStoreError> {
         let mut store = self.0.write().map_err(|_| InMemoryError::LockPoisoned)?;
         store.insert(blob.hash(), Bytes::copy_from_slice(blob.data()));
         Ok(())
     }
 
-    fn blob_delete(&self, key: Hash) -> Result<(), Self::Error> {
+    fn blob_delete(&self, key: Hash) -> Result<(), BlobStoreError> {
         let mut store = self.0.write().map_err(|_| InMemoryError::LockPoisoned)?;
         store.remove(&key);
         Ok(())
